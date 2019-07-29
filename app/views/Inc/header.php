68f4d990c3854d3a7ea98c44f99a6c094a370524
essemlalioussama <o.essemlali@gmail.com>
2019-07-29 01:05:32 +0100

    Ajouter CDN BOOTSTRAP

--- app/views/Inc/header.php
+++ app/views/Inc/header.php
@@ -5,7 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?php echo URLROOT.'/css/style.css';?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <title><?php echo SITENAME ;?></title>
 </head>
 <body>
+    <div class="container"> 
+        
+
+
+
+    
     
